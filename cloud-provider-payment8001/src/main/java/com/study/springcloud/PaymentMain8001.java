00bc2af83ed08a4dad6fb82cfbfee869aad2cd9f
faraway <lijialin_12@163.com>
2022-02-08 20:36:26 +0800

    commit

---
+++ cloud-provider-payment8001/src/main/java/com/study/springcloud/PaymentMain8001.java
@@ -0,0 +1,18 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> Title: PaymentMain8001 </p>
+ * <p> Description: </p>
+ *
+ * @author lijialin
+ * @since 2022-02-08
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
