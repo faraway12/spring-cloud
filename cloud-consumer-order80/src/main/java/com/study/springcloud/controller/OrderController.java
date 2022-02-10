5c42c56344887b5eddf6eba38c39b66ea2e79e5f
faraway <lijialin_12@163.com>
2022-02-10 10:49:47 +0800

    PostMapping

--- cloud-consumer-order80/src/main/java/com/study/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/study/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.study.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,7 +27,7 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/create")
+    @PostMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
