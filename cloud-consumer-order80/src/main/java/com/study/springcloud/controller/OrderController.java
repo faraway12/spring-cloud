5d12f6a724ef09f311acb22b4bd534d0cbd73999
faraway <lijialin_12@163.com>
2022-02-10 10:58:33 +0800

    请求体

--- cloud-consumer-order80/src/main/java/com/study/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/study/springcloud/controller/OrderController.java
@@ -3,10 +3,7 @@ package com.study.springcloud.controller;
 import com.study.springcloud.entities.CommonResult;
 import com.study.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -28,7 +25,7 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @PostMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    public CommonResult<Payment> create(@RequestBody Payment payment){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
 
