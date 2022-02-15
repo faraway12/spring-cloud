186c3990248bf1a1616ade3ac714533692b26598
faraway <lijialin_12@163.com>
2022-02-15 17:45:08 +0800

    consul

---
+++ cloud-consumer-services/cloud-consumer-order-zookeeper81/src/main/java/com/study/springcloud/controller/OrderController81.java
@@ -0,0 +1,31 @@
+package com.study.springcloud.controller;
+
+import com.study.springcloud.entities.CommonResult;
+import com.study.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * <p> Title: OrderController81 </p>
+ * <p> Description: </p>
+ *
+ * @author lijialin
+ * @since 2022-02-15
+ */
+@RestController
+@Slf4j
+public class OrderController81 {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/consumer/zookerper")
+    public CommonResult<Payment> query(@PathVariable("id") Long id){
+        return new CommonResult(200,"spring cloud zookerper,port:"+serverPort, UUID.randomUUID().toString());
+    }
+}
---
+++ cloud-provider-services/cloud-provider-payment-consul8004/src/main/java/com/study/springcloud/PaymentMain8004.java
@@ -0,0 +1,20 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * <p> Title: PaymentMain8004 </p>
+ * <p> Description: </p>
+ *
+ * @author lijialin
+ * @since 2022-02-15
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class,args);
+    }
+}
