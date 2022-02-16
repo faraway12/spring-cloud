406e3ef2a29e297ecaf25c3dd67daf045ba4ebad
faraway <lijialin_12@163.com>
2022-02-16 17:46:50 +0800

    合并

--- cloud-consumer-services/cloud-consumer-order-zookeeper81/src/main/java/com/study/springcloud/controller/OrderController81.java
+++ cloud-consumer-services/cloud-consumer-order-zookeeper81/src/main/java/com/study/springcloud/controller/OrderController81.java
@@ -5,7 +5,6 @@ import com.study.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
