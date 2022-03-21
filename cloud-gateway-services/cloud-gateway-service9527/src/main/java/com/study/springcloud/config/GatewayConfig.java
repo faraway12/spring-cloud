8e11279074761bfcb22a708f0cb853c57139f02d
faraway <lijialin_12@163.com>
2022-03-21 23:38:06 +0800

    断言&&自定义过滤器

--- cloud-gateway-services/cloud-gateway-service9527/src/main/java/com/study/springcloud/config/GatewayConfig.java
+++ cloud-gateway-services/cloud-gateway-service9527/src/main/java/com/study/springcloud/config/GatewayConfig.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.ZonedDateTime;
+
 /**
  * <p> Title: GatewayConfig </p>
  * <p> Description: </p>
@@ -17,6 +19,8 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
         RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
-        return routes.route("path_route_baidu",r->r.path("/guonei").uri("http://news.baidu.com")).build();
+        return routes.route("path_route_baidu",r->r.path("/guonei").and().after(ZonedDateTime.now()).uri("http://news.baidu.com")).build();
+
+//        return routes.route("path_route_baidu",r->r.path("/guonei").uri("http://news.baidu.com")).build();
     }
 }
