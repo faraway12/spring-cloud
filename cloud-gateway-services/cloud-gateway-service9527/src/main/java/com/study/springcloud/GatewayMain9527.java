210a520da9b39925e312d525b30830e8bd0fa6a5
faraway <lijialin_12@163.com>
2022-03-21 13:13:02 +0800

    gataway&&pom优化

---
+++ cloud-gateway-services/cloud-gateway-service9527/src/main/java/com/study/springcloud/GatewayMain9527.java
@@ -0,0 +1,20 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * <p> Title: GatewayMain9527 </p>
+ * <p> Description: </p>
+ *
+ * @author lijialin
+ * @since 2022-03-21
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class,args);
+    }
+}
