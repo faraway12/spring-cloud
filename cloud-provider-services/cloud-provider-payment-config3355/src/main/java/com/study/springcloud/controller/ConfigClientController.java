07297232ef452c77851f55e4e58039753afe4618
faraway <lijialin_12@163.com>
2022-03-22 20:07:49 +0800

    POST手动刷新

--- cloud-provider-services/cloud-provider-payment-config3355/src/main/java/com/study/springcloud/controller/ConfigClientController.java
+++ cloud-provider-services/cloud-provider-payment-config3355/src/main/java/com/study/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.study.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-03-22
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
