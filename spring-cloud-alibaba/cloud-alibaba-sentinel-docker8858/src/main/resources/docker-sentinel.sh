0c8076727dc3a8a7568930511a90f44997ef27a3
faraway <lijialin_12@163.com>
2022-05-16 22:08:14 +0800

    docker镜像制作步骤

--- spring-cloud-alibaba/cloud-alibaba-sentinel-docker8858/src/main/resources/docker-sentinel.sh
+++ spring-cloud-alibaba/cloud-alibaba-sentinel-docker8858/src/main/resources/docker-sentinel.sh
@@ -10,6 +10,8 @@ java -Dserver.port=8858 -Dcsp.sentinel.dashboard.server=localhost:8858 -Dproject
 --待后续自己制作一个docker镜像
 
 --已制作完成:
+docker build -t faraway12/sentinel-dashboard:latest .
+docker push faraway12/sentinel-dashboard:latest
 docker run -d --name sentinel-dashboard -p 8858:8858 faraway12/sentinel-dashboard
 
 --自己制作的docker镜像好像也不行 目前看macos只能在本地运行了 暂不使用docker
